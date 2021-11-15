8fd251ff4cfbff3e10630364e35b83512ae4c77b
Kyler Froman <kgamer5000@gmail.com>
2021-11-15 16:39:23 -0600

    Updated main page

--- nav.php
+++ nav.php
@@ -18,7 +18,7 @@
         <!-- navbar icons modified from https://freeicons.io/ -->
         <nav class="navbar navbar-expand-custom sticky-top">
             <div class="container-fluid">
-                <div id="home-link">
+                <div class="navigator" id="home">
                     <img src="Assets/icon.png" id="home-logo">
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsible">
@@ -26,57 +26,57 @@
                   </button>
                   <div class="collapse navbar-collapse" id="collapsible">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <p class="nav-link" id="rules">
+                        <li class="nav-item navigator" id="rules">
+                            <p class="nav-link">
                                 <img src="Assets/list.png" class="img-fluid barImg">
                                 <br>
                                 Rules
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="passions">
+                        <li class="nav-item navigator" id="passions">
+                            <p class="nav-link">
                                 <img src="Assets/pencil.png" class="img-fluid barImg">
                                 <br>
                                 Passions
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="races">
+                        <li class="nav-item navigator" id="races">
+                            <p class="nav-link">
                                 <img src="Assets/person.png" class="img-fluid barImg">
                                 <br>
                                 Races
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="classes">
+                        <li class="nav-item navigator" id="classes">
+                            <p class="nav-link">
                                 <img src="Assets/muscle.png" class="img-fluid barImg">
                                 <br>
                                 Classes
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="abilities">
+                        <li class="nav-item navigator" id="abilities">
+                            <p class="nav-link">
                                 <img src="Assets/stars.png" class="img-fluid barImg">
                                 <br>
                                 Abilities
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="feats">
+                        <li class="nav-item navigator" id="feats">
+                            <p class="nav-link">
                                 <img src="Assets/ribbon.png" class="img-fluid barImg">
                                 <br>
                                 Feats
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="weapons">
+                        <li class="nav-item navigator" id="weapons">
+                            <p class="nav-link">
                                 <img src="Assets/swords.png" class="img-fluid barImg">
                                 <br>
                                 Weapons
                             </p>
                         </li>
-                        <li class="nav-item">
-                            <p class="nav-link" id="artifacts">
+                        <li class="nav-item navigator" id="artifacts">
+                            <p class="nav-link">
                                 <img src="Assets/exclamation.png" class="img-fluid barImg">
                                 <br>
                                 Artifacts
