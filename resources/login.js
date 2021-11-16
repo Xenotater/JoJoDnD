c43f38f40d04acebf137c7bdfe3a36a3dc8a5da9
Kyler Froman <kgamer5000@gmail.com>
2021-11-16 04:38:20 -0600

    added form email functionality

--- resources/login.js
+++ resources/login.js
@@ -6,13 +6,19 @@ $(document).ready(function() {
     $("body").on("click", "#close", function() {
         close();
     })
+
+    $("#submit").click(function() {
+        if ($("#name").val() != "" && $("#subject").val() != "" && $("#comment").val() != "") {
+            $("#response-frame").css("display", "unset");
+        }
+    })
 })
 
 function popUp() {
     var newText = "<div id='popUp' class='center'><span id='close'><i class='bi bi-x-lg'></i></span>";
     newText += "<div class='content' id='login'>";
     newText += "<h2>Admin Login</h2>";
-    newText += "<form id='login-form' action='contacts.php' method='post'>";
+    newText += "<form id='login-form' action='admin.php' method='post'>";
     newText += "<label for='user'>Username: </label><input type='text' id='user' name='user'><br>";
     newText += "<label for='pass'>Password: </label><input type='password' id='pass' name='pass'><br>";
     newText += "<input type='submit' value='Login'>"
