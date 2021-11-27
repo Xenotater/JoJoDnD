59503632c1120f2901160c6be6c3820633da89db
Kyler Froman <kgamer5000@gmail.com>
2021-11-26 23:35:10 -0600

    improved page titles

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -1,8 +1,8 @@
-<!-- admin page to view contacts -->
 <!DOCTYPE html>
+<!-- admin page to view contacts -->
 <html lang="en">
     <head>
-        <title>Admin</title>
+        <title>Admin | JoJo D&D</title>
         <meta charset="utf-8">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
