85269d6ac16029c55df72f32b1939a08f3daaff4
Kyler Froman <kgamer5000@gmail.com>
2022-02-02 20:16:16 -0600

    added skills to editor & switched from html2canvas to html-to-dom

--- resources/editor/editor.js
+++ resources/editor/editor.js
@@ -4,18 +4,19 @@ $(document).ready(function () {
     $("input").on("keyup", function() {
         var val = $(this).width() / $(this).val().length, upper = 12, lower = 8;
 
-        if ($(this).attr("type") == "number") {
-            upper = 16;
-            lower = 12;
-        }
+        if (!$(this).hasClass("skill-bonus")) {
+            if ($(this).attr("type") == "number") {
+                upper = 16;
+                lower = 12;
+            }
 
-        console.log(val);
-        if (val > upper)
-            $(this).css("font-size", "24px");
-        else if (val < upper && val > lower)
-            $(this).css("font-size", "16px");
-        else
-            $(this).css("font-size", "12px");
+            if (val > upper)
+                $(this).css("font-size", "24px");
+            else if (val < upper && val > lower)
+                $(this).css("font-size", "16px");
+            else
+                $(this).css("font-size", "12px");
+        }
     });
 
     $("#dl-btn").click(function() {
@@ -26,28 +27,31 @@ $(document).ready(function () {
 //PDF generation assisted by https://www.freakyjolly.com/html2canvas-multipage-pdf-tutorial/
 function generatePDF() {
     var pdf;
-    html2canvas(document.querySelector("#page1")).then(function (canvas) {
-        var imgData = canvas.toDataURL("/image/png", 1.0);
+    domtoimage.toPng(document.querySelector("#page1")).then(function (imgData) {
         pdf = new jsPDF('p', 'in', 'letter');
         pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 11);
         console.log(imgData);
-    });
-    html2canvas(document.querySelector("#page2")).then(function (canvas) {
-        var imgData = canvas.toDataURL("/image/png", 1.0);
-        pdf.addPage('letter');
-        pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 11);
-    });
-    html2canvas(document.querySelector("#page3")).then(function (canvas) {
-        var imgData = canvas.toDataURL("/image/png", 1.0);
-        pdf.addPage('letter');
-        pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 11);
 
         setTimeout(function() {
-            pdf.save("JoJo-Character-Sheet.pdf");
-            var dl = "<a id='hidden-dl' href='JoJo-Character-Sheet.pdf' download>";
-            $("body").append(dl);
-            $("#hidden-dl").click();
-            $("#hidden-dl").remove();
+            domtoimage.toPng(document.querySelector("#page2")).then(function (imgData) {
+                pdf.addPage('letter');
+                pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 11);
+
+                setTimeout(function() {
+                    domtoimage.toPng(document.querySelector("#page3")).then(function (imgData) {
+                        pdf.addPage('letter');
+                        pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 11);
+                
+                        setTimeout(function() {
+                            pdf.save("JoJo-Character-Sheet.pdf");
+                            var dl = "<a id='hidden-dl' href='JoJo-Character-Sheet.pdf' download>";
+                            $("body").append(dl);
+                            $("#hidden-dl").click();
+                            $("#hidden-dl").remove();
+                        }, 0);
+                    });
+                }, 0);
+            });
         }, 0);
     });
 }
