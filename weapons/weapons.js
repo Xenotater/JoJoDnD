0ba29d25455f7c9697ddf73564ab3448a8fdddd9
Xenotater <kgamer5000@gmail.com>
2023-01-26 00:54:03 -0600

    imported new weapons for rework

--- weapons/weapons.js
+++ weapons/weapons.js
@@ -84,9 +84,10 @@ function createTable() {
     newText += "<th><div class='label attr'>Name</div><div class='sortBtns'><i id='nameUp' class='bi bi-caret-up sorter'></i><br><i id='nameDown' class='bi bi-caret-down sorter'></i></div></th>";
     newText += "<th><div class='label attr'>Attributes</div><div class='sortBtns'><i id='attrUp' class='bi bi-caret-up sorter'></i><br><i id='attrDown' class='bi bi-caret-down sorter'></i></div></th>";
     newText += "<th><div class='label attr'>Type</div><div class='sortBtns'><i id='typeUp' class='bi bi-caret-up sorter'></i><br><i id='typeDown' class='bi bi-caret-down sorter'></i></div></th>";
-    newText += "<th><div class='label attr'>Stat</div><div class='sortBtns'><i id='statUp' class='bi bi-caret-up sorter'></i><br><i id='statDown' class='bi bi-caret-down sorter'></i></div></th>";
+    newText += "<th><div class='label attr'>Specialization</div><div class='sortBtns'><i id='specUp' class='bi bi-caret-up sorter'></i><br><i id='specDown' class='bi bi-caret-down sorter'></i></div></th>";
+    newText += "<th><div class='label attr'>Stat/DC</div><div class='sortBtns'><i id='statUp' class='bi bi-caret-up sorter'></i><br><i id='statDown' class='bi bi-caret-down sorter'></i></div></th>";
     newText += "<th><div class='label attr'>Prerequisite</div><div class='sortBtns'><i id='prereqUp' class='bi bi-caret-up sorter'></i><br><i id='prereqDown' class='bi bi-caret-down sorter'></i></div></th>";
-    newText += "<th><div class='label attr'>Effect</div><div class='sortBtns'><i id='effectUp' class='bi bi-caret-up sorter'></i><br><i id='effectDown' class='bi bi-caret-down sorter'></i></div></th>";
+    newText += "<th><div class='label attr'>Damage</div><div class='sortBtns'><i id='dmgUp' class='bi bi-caret-up sorter'></i><br><i id='dmgDown' class='bi bi-caret-down sorter'></i></div></th>";
     newText += "</tr></thead><tbody></tbody></table></div>";
 
     $("#weapon-list").html(newText);
@@ -108,7 +109,7 @@ function updateTable() {
             if (j < w.attr.length - 1)
                 newText += ", "
         }
-        newText += "</td><td>" + w.type + "</td><td>" + w.stat + "</td><td>" + w.prereq + "</td><td>" + w.effect + "</td></tr>";
+        newText += "</td><td>" + w.type + "</td><td>" + w.spec + "</td><td>" + w.stat + "</td><td>" + w.prereq + "</td><td>" + w.dmg + "</td></tr>";
     }
     newText += "</tbody></table>";
 
@@ -116,54 +117,53 @@ function updateTable() {
 }
 
 function hint(attr) {
-    if (attr.includes("Reach")) {
-        var reach = attr.charAt(7);
-        attr = "Reach";
-    }
     if (attr.includes("Thrown") || attr.includes("Ranged")) {
-        var dist = attr.substring(attr.indexOf('(') + 1, attr.indexOf(')')).split('/');
+        var range = attr.substring(attr.indexOf('(') + 1, attr.indexOf(')')).split('/');
         attr = attr.substring(0, attr.indexOf(' '));
     }
-    if (attr.includes("Reload")) {
+    if (attr.includes("Reach") || attr.includes("Radius")) {
+        var dist = attr.substring(attr.indexOf('(') + 1, attr.indexOf(')'));
+        attr = attr.substring(0, attr.indexOf(' '));
+    }
+    if (attr.includes("Reload")  || attr.includes("Burst")) {
         var ammo = attr.substring(attr.indexOf('x') + 1, attr.indexOf(')'));
-        attr = "Reload";
+        attr = attr.substring(0, attr.indexOf(' '));
     }
-    if (attr.includes("Explosive")) {
-        var dc = attr.substring(attr.indexOf('(') + 1, attr.indexOf(')')).replace("DC ", "");
-        attr = "Explosive";
-    }
-    if (attr.includes("Burst")) {
-        var num = attr.substring(attr.indexOf('x') + 1, attr.indexOf(')'));
-        attr = "Burst";
+    if (attr.includes("Concealed") || attr.includes("Versatile")) {
+        var bonus = attr.substring(attr.indexOf('(') + 1, attr.indexOf(')'));
+        attr = attr.substring(0, attr.indexOf(' '));
     }
 
     switch(attr) {
         case "Name": 
             return("The name of the weapon.");
         case "Attributes":
-            return("The qualities that the weapon has.");
+            return("The properties that the weapon has.");
         case "Type":
             return("The type of weapon for the purpose of Proficiencies.");
-        case "Stat":
-            return("The Attack Stat of the weapon. Added to both to-hit AND damage rolls.");
+        case "Specialization":
+            return("The type of the weapon for the purpose of Specialization Feats.")
+        case "Stat/DC":
+            return("The Attack Stat or DC of the weapon. The Attack Stat is added to both to-hit AND damage rolls. If the weapon has a DC, a Dex Save must be rolled and half damage is taken on success.");
         case "Prerequisite":
             return("The requirement that must be met in order to use the weapon.");
-        case "Effect":
-            return("The damage and/or additional effect of the weapon.");
+        case "Damage":
+            return("The damage the weapon deals when it hits.");
         case "Melee":
             return("This weapon has a range of 1m from the person/stand wielding it.");
         case "Reach":
-            return("This weapon has a range of " + reach + "m from the person/stand wielding it.");
-        case "One-Handed":
-            return("This weapon can be wielded with one hand. When making an attack using a One-Handed weapon with another in your other hand, you may make an additional Attack with the other weapon as a Bonus Action.<br>May be hostered or concealed easily.");
+            return("This weapon has a range of " + dist + " from the person/stand wielding it.");
         case "Two-Handed":
             return("This weapon requires two hands to wield. Using this weapon to make an Attack with only 1 hand inflicts Disadvantage.<br>Difficult to holster or conceal.");
         case "Thrown":
-            return("This weapon can be used as both a ranged and a melee weapon. It can be thrown up to " + dist[0] + "m, or as far as " + dist[1] + "m with Disadvantage.<br>Once thrown, this weapon must be picked up in order to be used again.");
+            if (range[0] != range[1])
+                return("This weapon can be thrown up to " + range[0] + "m, or as far as " + range[1] + "m with Disadvantage.<br>Once thrown, this weapon must be picked up in order to be used again (unless it is an explosive, in which case it is destroyed).");
+            else
+                return("This weapon can be thrown up to " + range[0] + "m.<br>Once thrown, this weapon must be picked up in order to be used again (unless it is an explosive, in which case it is destroyed).")
         case "Ranged":
-            return("This weapon can fire up to " + dist[0] + "m, or as far as " + dist[1] + "m with Disadvantage.<br>Firing within melee range imposes Disadvantage.");
+            return("This weapon can fire up to " + range[0] + "m, or as far as " + range[1] + "m with Disadvantage.<br>Firing within melee range imposes Disadvantage.");
         case "Firearm":
-            return("This weapon does NOT impose Disadvantage when fired in melee range.<br>Often makes a loud sound which can be heard up to 0.5km away.");
+            return("This weapon makes a loud sound which can be heard up to 0.5km away.<br>It also jams if a natural 1 is rolled while using it.<br>This weapon's ammuntion doesn't function when wet.");
         case "Reload":
             return("This weapon can be used " + ammo + " times before needing to be reloaded.<br>Reloading takes a full Attack.");
         case "Shield":
@@ -171,7 +171,33 @@ function hint(attr) {
         case "Explosive":
             return("This weapon is destroyed once used. Damage is dealt in a radius around the target.<br>Explosives may be used to set traps, not just as thrown/propelled weapons.<br>Those caught within the blast radius make a Dex Save with a DC of " + dc + ", taking half damage on success.<br>If a 1 is rolled while throwing, the explosive goes off centered on the user.");
         case "Burst":
-            return("This weapon consumes " + num + " of it's ammo each time it is fired.")
+            return("This weapon consumes " + ammo + " of it's ammo each time it is fired.")
+        case "Concealed":
+            return("This weapon grants " + bonus + " to any Check made to conceal it.");
+        case "Light":
+            return("This weapon is light and easy to handle, allowing you to wield another weapon in your other hand.");
+        case "Remote":
+            return("This weapon can be detonated as a Bonus Action from up to 1km away.");
+        case "Timer":
+            return("A timer may be set on this weapon, detonating it once the timer runs out. The timer may be set for up to 12 hours.");
+        case "Radius":
+            return("This weapon forces all creatures within " + dist + " of the target to make a Dexterity Saving Throw, taking half damage on success and full damage on failure.");
+        case "Siege":
+            return ("This weapon deals double damage to objects.");
+        case "Reliable":
+            return("This weapon does not jam if you roll a natural 1.");
+        case "Point Blank":
+            return("This weapon does not grant Disadvantage if fired within melee range.<br>Additionally, this weapon grants Advantage when aiming at a Prone target within melee range.");
+        case "Ambush":
+            return("This weapon always crits when attacking a Surprised target.");
+        case "Bulky":
+            return("This weapon must be braced using a Bonus Action in order to begin firing.");
+        case "Complex":
+            return("This weapon takes a total of three Attacks to reload.");
+        case "Flame":
+            return("When a targeted creature or object takes fire damage from this weapon, it is set ablaze (assuming the target is flammable).");
+        case "Versatile":
+            return("This weapon may be wielded with one or two hands. If two hands are used, it's damage becomes " + bonus + ".");
         default:
             return("Error: This attribute doesn't have a definition. Please alert the site administrator.");     
     }
@@ -195,11 +221,11 @@ function popUp(text, posX, posY) {
 function sort() {
     weapons.sort(function (a, b) {
         var x = a[sortType]; var y = b[sortType];
-        if (x == "None")
+        if (x == "None" || x.includes("DC"))
             x = "A";
-        if (y == "None")
+        if (y == "None" || y.includes("DC"))
             y = "A";
-        if (sortType == "effect") {
+        if (sortType == "dmg") {
             x = diceParse(x);
             y = diceParse(y);
         }
