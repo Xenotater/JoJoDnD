21e2943d37195874befd6e233abfd0f437dc7cc1
Xenotater <kgamer5000@gmail.com>
2024-07-23 23:10:33 -0500

    community resource updates

--- resources/community/resources.js
+++ resources/community/resources.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
   getData();
+
+  $("body").click(function() {
+    $(".versionSelector[style*='display: flex;']").css("display", "none"); //hide any showing version selectors
+  });
+
+  $("#cards").on("click", ".hasVersions", function(e) {
+    const selector = this.children[0];
+    if ($(selector).css("display") === "flex")
+      $(selector).css("display", "none");
+    else
+      $(selector).css("display", "flex"); //show matching version selector
+    e.stopPropagation();
+  });
+
+  $("#cards").on("click", ".versionSelector", function(e) {
+    e.stopPropagation();
+  });
 });
 
 function getData() {
@@ -12,11 +29,24 @@ function getData() {
 
 function updateResources(resources) {
   resources.forEach(r => {
-    let newText = "<a target='_blank' href='" + r.link + "' class='card'>";
+    let newText = "";
+    
+    if (r.link)
+      newText += "<a target='_blank' href='" + r.link + "' class='card'>";
+    
+    if (r.versions) {
+      newText += "<div class='card hasVersions'>";
+      newText += "<div class='versionSelector'>";
+      for (let i=0; i<r.versions.length; i++)
+        newText += "<a target='_blank' href='" + r.versions[i].link + "' class='version'>" + r.versions[i].name + "</a>";
+      newText += "</div>";
+    }
+    
     newText += "<div class='title'>" + r.title;
     newText += "</div><div class='cardImg'><img src='" + r.img + "' alt = '" + r.alt + "'></img>";
     newText += "</div><div class='desc'>" + r.desc;
     newText += "</div></a>";
+
     $("#cards").append(newText);
   });
 }
