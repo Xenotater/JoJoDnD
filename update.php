4129c522423bbf7968844bbdb2c2cf3ab40027e3
Xenotater <kgamer5000@gmail.com>
2024-09-10 21:22:16 -0500

    missed changes

--- update.php
+++ update.php
@@ -10,7 +10,7 @@
   $_SESSION["checkedUpdate"] = true;
 
   $version = $_POST["version"];
-  $latest = "1.12.2.0";
+  $latest = "1.12.2.1";
 
   if ($version != $latest) {
     header("Clear-Site-Data: \"cache\"");
