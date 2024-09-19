27c11fc900fc5068ee69d9b90e9ff9db11c2d289
Xenotater <kgamer5000@gmail.com>
2024-09-18 22:41:02 -0500

    new resource

--- update.php
+++ update.php
@@ -10,7 +10,7 @@
   $_SESSION["checkedUpdate"] = true;
 
   $version = $_POST["version"];
-  $latest = "1.12.2.2";
+  $latest = "1.12.2.3";
 
   if ($version != $latest) {
     header("Clear-Site-Data: \"cache\"");
