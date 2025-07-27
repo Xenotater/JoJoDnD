e29ee7d9483de7ca5c1f3d9685ccf95c05de6167
Xenotater <kgamer5000@gmail.com>
2025-07-27 01:31:15 -0500

    more reasonable text size on not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,10 +4,10 @@ export default function NotFound() {
   return (
     <div className="flex flex-col h-(--fullContentHeight) justify-center items-center">
       <FancyImage className="mb-8" src="/not-found/dio.webp" alt="It was I, Dio!" width={666} height={375} type={"popout"}/>
-      <h3 className="text-center">
+      <h4 className="text-center">
         Sorry, looks like there&apos;s nothing here. Either this page doesn&apos;t exist or you&apos;ve entered the wrong URL.
         <br></br>If you think this is an error, contact an administrator.
-      </h3>
+      </h4>
     </div>
   );
 }
