2972660d57d454ba83d9492554c61f00e201ea94
Xenotater <kgamer5000@gmail.com>
2026-02-15 15:46:45 -0600

    dont use prebuilt signin page

--- src/app/Components/Auth/AuthTest.tsx
+++ src/app/Components/Auth/AuthTest.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function AuthTest() {
   const {data: session} = useSession();
 
   return (
-    <div>
+    <form action={async (formData) => {
+      await signIn("credentials", {redirect: false, username: formData.get("username"), password: formData.get("password")})
+    }}>
       <p>Test Auth:</p>
-      <button onClick={() => signIn()}>Sign In</button><button onClick={() => signOut()}>Sign Out</button><br/>
-      {session?.user &&
-        <b>Hello {session?.user?.name}</b>
+      {session?.user ?
+        <><b>Hello {session?.user?.name}</b><br/><button onClick={() => signOut()}>Sign Out</button></>
+        :
+        <>
+          <input required name="username" type="text"/>
+          <input required name="password" type="password"/>
+          <button type="submit">Sign In</button>
+        </>
       }
-    </div>
+    </form>
   );
 }
