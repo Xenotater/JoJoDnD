b1b33afe00194b4e0c93195864bc41fa4bc9d8e8
Xenotater <kgamer5000@gmail.com>
2026-04-16 21:21:34 -0500

    missed cleanup

--- src/app/Components/Layout/ScalingInput/ScalingInput.tsx
+++ src/app/Components/Layout/ScalingInput/ScalingInput.tsx
@@ -5,7 +5,7 @@ import { Textfit } from "react-textfit";
 
 import styles from "./ScalingInput.module.css";
 
-export default function ScalingInput(props: {className?: string, fontMin?: number, fontMax?: number} & React.InputHTMLAttributes<HTMLInputElement>) {
+export default function ScalingInput(props: {className?: string, fontmin?: number, fontmax?: number} & React.InputHTMLAttributes<HTMLInputElement>) {
   const [editing, setEditing] = useState(false);
   const [hovering, setHovering] = useState(false);
 
@@ -13,29 +13,41 @@ export default function ScalingInput(props: {className?: string, fontMin?: numbe
   const textRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    const input = inputRef.current;
+      const text = textRef.current;
+
     const toggleEdit = (state: boolean) => {
-      console.log("edit: " + state);
       setEditing(state);
       if (inputRef.current && state)
         inputRef.current.focus();
     };
     const toggleHover = (state: boolean) => {
-      console.log("hover: " + state);
       setHovering(state);
     };
 
-    if (inputRef.current) {
-      const input = inputRef.current;
+    if (input) {
       input.addEventListener("blur", () => toggleEdit(false));
       input.addEventListener("focus", () => toggleEdit(true));
       input.addEventListener("mouseleave", () => toggleHover(false));
     }
 
-    if (textRef.current) {
-      const text = textRef.current;
+    if (text) {
       text.addEventListener("focus", () => toggleEdit(true));
       text.addEventListener("mouseenter", () => toggleHover(true));
     }
+
+    return(() => {
+      if (input) {
+        input.removeEventListener("blur", () => toggleEdit(false));
+        input.removeEventListener("focus", () => toggleEdit(true));
+        input.removeEventListener("mouseleave", () => toggleHover(false));
+      }
+
+      if (text) {
+        text.removeEventListener("focus", () => toggleEdit(true));
+        text.removeEventListener("mouseenter", () => toggleHover(true));
+      }
+    });
 
   }, [inputRef, textRef]);
 
@@ -44,7 +56,7 @@ export default function ScalingInput(props: {className?: string, fontMin?: numbe
       <input {...props} ref={inputRef} className={`${props.className} ${editing || hovering ? "" : styles.hidden}`}/>
       <div ref={textRef} tabIndex={0} className={`${props.className} ${editing || hovering ? styles.hidden : ""}`}>
         {!(editing || hovering) && 
-          <Textfit className={styles.inherit} max={props.fontMax} min={props.fontMin}>{props.value}</Textfit>
+          <Textfit className={styles.inherit} max={props.fontmax} min={props.fontmin}>{props.value}</Textfit>
         }
       </div>
     </>
--- src/app/resources/editor/Components/StandardSheets/Page1.tsx
+++ src/app/resources/editor/Components/StandardSheets/Page1.tsx
@@ -17,13 +17,13 @@ export default function StandardPage1({data, updateField}: {data: Partial<Charac
       </div>
       <div className="w-full flex gap-[24px]">
         <InputWrapper label="Character Name" className="basis-1/2 min-w-0">
-          <ScalingInput className={styles.bigInput} fontMax={24} value={data.name} onChange={(e) => updateField("name", e.target.value)}/>
+          <ScalingInput className={styles.bigInput} fontmax={24} value={data.name} onChange={(e) => updateField("name", e.target.value)}/>
         </InputWrapper>
         <InputWrapper label="Hit Point Max" className="basis-1/6 min-w-0">
-          <ScalingInput className={styles.bigInput} fontMax={24} value={data.maxHP} onChange={(e) => updateField("maxHP", e.target.value)}/>
+          <ScalingInput className={styles.bigInput} fontmax={24} value={data.maxHP} onChange={(e) => updateField("maxHP", e.target.value)}/>
         </InputWrapper>
         <InputWrapper label="Current Hit Points" className="basis-1/3 min-w-0">
-          <ScalingInput className={styles.bigInput} fontMax={24} value={data.hp} onChange={(e) => updateField("hp", e.target.value)}/>
+          <ScalingInput className={styles.bigInput} fontmax={24} value={data.hp} onChange={(e) => updateField("hp", e.target.value)}/>
         </InputWrapper>
       </div>
     </div>
